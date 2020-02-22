ba2f58997d50eab93f379aeeb0159e6c45023e1f
Frank Uzoka <Frankuzoka@hotmail.com>
2020-02-22 09:34:27 -0500

    DO LOGIC

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,6 +5,15 @@ public class App {
     }
 
 
-    public static void addLogic(){}
+    public static void addLogic(){
+
+        System.out.println("THE WORST NUMBER GUESSING GAME EVER!!!!!!!!!\n");
+        System.out.println("I\"M THIKING OF A NUMBER BETWEEN 1-10.  TRY 2 GUESS IT!");
+
+
+
+
+
+    }
 
 }
