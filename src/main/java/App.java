e7e73fc85bfef3ccf133b18a2bb80431282f33ba
Frank Uzoka <Frankuzoka@hotmail.com>
2020-02-22 09:35:03 -0500

    Bring in scanner

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class App {
 
     public static void main(String[] args) {
@@ -9,6 +11,8 @@ public class App {
 
         System.out.println("THE WORST NUMBER GUESSING GAME EVER!!!!!!!!!\n");
         System.out.println("I\"M THIKING OF A NUMBER BETWEEN 1-10.  TRY 2 GUESS IT!");
+
+        Scanner scan = new Scanner(System.in);
 
 
 
